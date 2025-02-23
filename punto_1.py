ba07845c3889dd93b28c4748171dd17160f2a2bd
Shara García <shara.garcia.betancur@gmail.com>
2025-02-23 17:15:10 -0500

    Matrix 6x6 y suma de números que almacena en ella

--- punto_1.py
+++ punto_1.py
@@ -1,1 +1,36 @@
-print("Hello World!!")
+# Realice un algoritmo que almacene números en una matriz de 6 * 6.
+# Imprimir la suma de los números almacenados en la matriz.
+from random import randint
+
+
+def generar_matriz(filas, columnas):
+    matriz = []
+
+    for i in range(filas):
+        fila = []
+        for j in range(columnas):
+            fila.append(randint(1, 100))
+        matriz.append(fila)
+
+    return matriz
+
+
+def imprimir_matriz(matriz):
+    for i in matriz:
+        print(i)
+
+
+def suma_matriz(matriz):
+    suma = sum([sum(i) for i in matriz])
+    return suma
+
+
+def main():
+    matriz = generar_matriz(6, 6)
+    imprimir_matriz(matriz)
+    print(f"La suma de los números almacenados en la matriz es: {
+            suma_matriz(matriz)}")
+
+
+if __name__ == "__main__":
+    main()
