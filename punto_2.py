c94994c237ab024d6c676ca02e809df18524e780
Shara García <shara.garcia.betancur@gmail.com>
2025-02-23 17:50:50 -0500

    Matrix 5x5 que determine la posición del num max

---
+++ punto_2.py
@@ -0,0 +1,48 @@
+# Realice un algoritmo que llene una matriz de 5 * 5 y determine la
+# posición [fila ,columna] del número mayor almacenado en la matriz.
+from random import randint
+
+
+def generar_matriz(filas, columnas):
+    matriz = []
+
+    for i in range(filas):
+        fila = []
+        for j in range(columnas):
+            fila.append(randint(1, 100))
+        matriz.append(fila)
+
+    return matriz
+
+
+def imprimir_matriz(matriz):
+    for i in matriz:
+        print(i)
+
+
+def encontrar_maximo(matriz):
+    max_valor = matriz[0][0]
+    posicion = (0, 0)
+
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] > max_valor:
+                max_valor = matriz[i][j]
+                posicion = (i, j)
+
+    # Ajustar posición comenzando en 1
+    posicion = (posicion[0] + 1, posicion
+                [1] + 1)  # (fila, columna)
+
+    return max_valor, posicion
+
+
+def main():
+    matriz = generar_matriz(5, 5)
+    imprimir_matriz(matriz)
+    max_valor, posicion = encontrar_maximo(matriz)
+    print(f"El número mayor almacenado en la matriz es: {max_valor} en la posición {posicion}")
+
+
+if __name__ == "__main__":
+    main()
