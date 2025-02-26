ca08dde6c4aa6be11296a1f7d3e5a7a03fda7cb6
Shara García <shara.garcia.betancur@gmail.com>
2025-02-26 15:00:54 -0500

    Determinar el inventario total de una matriz de objetos de un producto

--- Preparatorio - Matrices/punto_2.py
+++ Preparatorio - Matrices/punto_2.py
@@ -11,7 +11,7 @@ class Producto:
         self.cantidad = cantidad
 
     def __repr__(self):
-        return f'Producto("{self.nombre}", "Precio: {self.precio}", "Cantidad: {self.cantidad}")'
+        return f'Producto("{self.nombre}", Precio: {self.precio}, Cantidad: {self.cantidad})'
 
 
 # Función para calcular el inventario total
@@ -32,9 +32,9 @@ def imprimir_matriz(matriz):
 def main():
     # Crear la matriz de productos
     matriz_productos = [
-        [Producto("Producto 1", 100, 10), Producto("Producto 2", 200, 20), Producto("Producto 3", 300, 30)],
-        [Producto("Producto 4", 400, 40), Producto("Producto 5", 500, 50), Producto("Producto 6", 600, 60)],
-        [Producto("Producto 7", 700, 70), Producto("Producto 8", 800, 80), Producto("Producto 9", 900, 90)]
+        [Producto("iPhone 16 Plus", 100, 10), Producto("MacBook Air", 200, 20), Producto("iPad", 300, 30)],
+        [Producto("iPhone 15 Pro Max", 400, 40), Producto("MacBook Pro", 500, 50), Producto("Apple Watch Ultra", 600, 60)],
+        [Producto("AirTag", 700, 70), Producto("iMac", 800, 80), Producto("AirPods Pro", 900, 90)]
     ]
 
     print("Matriz de productos:")
