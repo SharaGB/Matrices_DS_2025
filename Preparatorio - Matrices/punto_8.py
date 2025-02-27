ca85a1ef0c7fe0a97fff994fd7dd3fb7909470f0
Shara García <shara.garcia.betancur@gmail.com>
2025-02-27 17:38:50 -0500

    Matriz que agrupa los productos de un almacén por categoría

--- Preparatorio - Matrices/punto_8.py
+++ Preparatorio - Matrices/punto_8.py
@@ -1,4 +1,4 @@
-# Problema: Dada una lista de productos con atributos nombre, peso,
+# Dada una lista de productos con atributos nombre, peso,
 # categoría, llena una matriz que represente las estanterías de un almacén.
 # Cada producto debe ubicarse en la sección correspondiente según su categoría.
 # Solución: Agrupa los productos por categoría en vectores y
